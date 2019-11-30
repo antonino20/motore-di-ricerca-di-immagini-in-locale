c52756b766ecc07dcd91464cba4feb1133f3a14b
steve <steven.97@tiscali.it>
2019-11-30 11:00:27 +0100

    Update style.js

--- style.js
+++ style.js
@@ -2,7 +2,6 @@ menu = document.getElementById('menu-vertical');
 iconoMenu = document.getElementById('icono-menu');
 
 iconoMenu.addEventListener("click", function(){
-	console.log("prueba");
 	listaMenu = document.getElementById('items');
 	if (listaMenu.style.display == "block"){
 		listaMenu.style.display="none";}
